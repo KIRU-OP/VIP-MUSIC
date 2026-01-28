10a499c2e1b37d1af170aa9d9ed6a70e5ef0e311
KIRU-OP <np564605@gmail.com>
2026-01-28 23:08:57 +0530

    Upload VIPMUSIC/utils/__init__.py

---
+++ VIPMUSIC/utils/__init__.py
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
+#
+# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# and is released under the MIT License.
+# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+from .afkdb import *
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .nightmodedb import *
+from .pastebin import *
+from .readable_time import *
+from .sys import *
