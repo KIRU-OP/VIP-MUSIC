9b59c28021f37e385593296c1d194a89ffe0778c
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-19 16:20:37 +0530

    Change default UPSTREAM_BRANCH from 'main' to 'master'

--- config/config.py
+++ config/config.py
@@ -65,7 +65,7 @@ PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-VIP
 # 🚀 UPSTREAM & DEPLOYMENT
 # ================================================================
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/lll-DEADLY-VENOM-lll/VIPMUSIC")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
